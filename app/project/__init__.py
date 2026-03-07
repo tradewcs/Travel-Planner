896c85d5a1b22978ed41215129bca434c3ccfdf0
Mike <tradewcs@gmail.com>
2026-03-07 14:21:24 +0200

    make project core structure

---
+++ app/project/__init__.py
@@ -0,0 +1,11 @@
+from src.project import Project, ProjectStatus
+from src.place import Place
+from src.project_place import ProjectPlace
+
+__all__ = [
+    "BaseModel",
+    "Project",
+    "ProjectStatus",
+    "Place",
+    "ProjectPlace",
+]
